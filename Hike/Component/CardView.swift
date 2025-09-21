5aa8b45142e0c3d345f4a961e8f52b14bf4ad5bb
Daniiar Erkinov <dan4111k1@gmail.com>
2025-09-21 13:17:30 +0600

    Add header component

--- Hike/Component/CardView.swift
+++ Hike/Component/CardView.swift
@@ -12,23 +12,66 @@ struct CardView: View {
         ZStack {
             CustomBackgroundView()
             
-            ZStack {
-                Circle()
-                    .fill(
-                        LinearGradient(
-                            colors: [
-                                Color.customIndigoMedium,
-                                Color.customSalmonLight
-                            ],
-                            startPoint: .topLeading,
-                            endPoint: .bottomTrailing
-                        )
-                    )
-                    .frame(width: 256, height: 256)
+            VStack {
+                // MARK: - HEADER
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("Hiking")
+                            .fontWeight(.black)
+                            .font(.system(size: 52))
+                            .foregroundStyle(
+                                LinearGradient(
+                                    colors: [
+                                        .customGrayLight,
+                                        .customGrayMedium
+                                    ],
+                                    startPoint: .top,
+                                    endPoint: .bottom
+                                )
+                            )
+                        
+                        Spacer()
+                        
+                        Button {
+                            // ACTION: Show a Sheet
+                            print("The button was pressed.")
+                        } label: {
+                            CustomButtonView()
+                        }
+                    }
+                    
+                    Text("Fun and enjoyable outdoor activity for friends and families.")
+                        .multilineTextAlignment(.leading)
+                        .italic()
+                        .foregroundColor(.customGrayMedium)
+                } //: HEADER
+                .padding(.horizontal, 30)
                 
-                Image("image-1")
-                    .resizable()
-                    .scaledToFit()
+                // MARK: - MAIN CONTENT
+                
+                
+                
+                ZStack {
+                    Circle()
+                        .fill(
+                            LinearGradient(
+                                colors: [
+                                    Color.customIndigoMedium,
+                                    Color.customSalmonLight
+                                ],
+                                startPoint: .topLeading,
+                                endPoint: .bottomTrailing
+                            )
+                        )
+                        .frame(width: 256, height: 256)
+                    
+                    Image("image-1")
+                        .resizable()
+                        .scaledToFit()
+                }
+                
+                // MARK: - FOOTER
+                
             }
         } //: CARD
         .frame(width: 320, height: 570)
