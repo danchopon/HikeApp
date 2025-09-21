9955fd74e63e0f7970a8eed09ac9c595e4f189a3
Daniiar Erkinov <dan4111k1@gmail.com>
2025-09-21 11:24:33 +0600

    initial commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Daniiar Erkinov on 21/9/25.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
